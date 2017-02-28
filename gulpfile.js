ee2c3b078793a2044a7761a5d4f729ba085278b3
alu0100851236 <alu0100851236@ull.edu.es>
2017-02-28 19:52:11 +0000

    C4 - Modificación del gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,21 @@ var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 var shell = require('gulp-shell');
 
+gulp.task('gh-pages', function() {
+  return gulp.src(['txt/*/*/*','txt/*/*','txt/*'])
+  .pipe(ghPages());
+});
+
 gulp.task('build', shell.task([
   'gitbook install', 'npm run build'
+]));
+
+gulp.task('deploy', shell.task([
+  'npm run deploy'
+]));
+
+gulp.task('serve', shell.task([
+  'gitbook serve'
 ]));
 
 gulp.task('default', ['build', 'deploy']);
